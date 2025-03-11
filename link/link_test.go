16dbc091b9a875d7bd3aa61adcef2d718ce14e35
Anadi Sharma <aanadi@cisco.com>
2025-03-11 19:11:04 -0400

    More changes

--- link/link_test.go
+++ link/link_test.go
@@ -189,7 +189,7 @@ func testLink(t *testing.T, link Link, prog *ebpf.Program) {
 		// ownership.
 		dupFD := testutils.DupFD(t, fder.FD())
 
-		newLink, err := NewFromID(dupFD)
+		newLink, err := NewFromFD(dupFD)
 		testutils.SkipIfNotSupported(t, err)
 		if err != nil {
 			t.Fatal("Can't create new link from dup link FD:", err)
