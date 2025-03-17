5c0f13a65507c65f51f5334610b9569ff1a83edc
Lorenz Bauer <lmb@isovalent.com>
2025-03-17 15:17:21 -0700

    ebpf: add WindowsAttachTypeForGUID
    Export a function which allows resolving a GUID to an AttachType.
    This is useful when an eBPF extension has not yet registered a stable
    AttachType with the eBPF for Windows runtime, as is currently the
    case for ntosebpfext.
    
    Signed-off-by: Lorenz Bauer <lmb@isovalent.com>

--- types_windows.go
+++ types_windows.go
@@ -10,47 +10,24 @@ import (
 	"github.com/cilium/ebpf/internal/platform"
 )
 
-// WindowsProgramTypeForGUID resolves a GUID to a ProgramType.
-//
-// The GUID must be in the form of "{XXXXXX-XXXX-XXXX-XXXX-XXXXXXXXXXXX}".
-//
-// Returns an error wrapping [os.ErrNotExist] if the GUID is not recignized.
-func WindowsProgramTypeForGUID(guid string) (ProgramType, error) {
-	progTypeGUID, err := windows.GUIDFromString(guid)
-	if err != nil {
-		return 0, fmt.Errorf("parse GUID: %w", err)
-	}
-
-	rawProgramType, err := efw.EbpfGetBpfProgramType(progTypeGUID)
-	if err != nil {
-		return 0, fmt.Errorf("get program type: %w", err)
-	}
-
-	if rawProgramType == 0 {
-		return 0, fmt.Errorf("program type not found for GUID %v: %w", guid, os.ErrNotExist)
-	}
-
-	return ProgramTypeForPlatform(platform.Windows, rawProgramType)
-}
-
 // WindowsAttachTypeForGUID resolves a GUID to an AttachType.
 //
 // The GUID must be in the form of "{XXXXXX-XXXX-XXXX-XXXX-XXXXXXXXXXXX}".
 //
 // Returns an error wrapping [os.ErrNotExist] if the GUID is not recignized.
 func WindowsAttachTypeForGUID(guid string) (AttachType, error) {
-	attachTypeGUID, err := windows.GUIDFromString(guid)
+	tmp, err := windows.GUIDFromString(guid)
 	if err != nil {
 		return 0, fmt.Errorf("parse GUID: %w", err)
 	}
 
-	rawAttachType, err := efw.EbpfGetBpfAttachType(attachTypeGUID)
+	rawAttachType, err := efw.EbpfGetBpfAttachType(tmp)
 	if err != nil {
 		return 0, fmt.Errorf("get attach type: %w", err)
 	}
 
 	if rawAttachType == 0 {
-		return 0, fmt.Errorf("attach type not found for GUID %v: %w", attachTypeGUID, os.ErrNotExist)
+		return 0, fmt.Errorf("attach type not found for GUID %v: %w", tmp, os.ErrNotExist)
 	}
 
 	return AttachTypeForPlatform(platform.Windows, rawAttachType)
--- types_windows_test.go
+++ types_windows_test.go
@@ -8,20 +8,6 @@ import (
 	"golang.org/x/sys/windows"
 )
 
-func TestWindowsProgramTypeForGUID(t *testing.T) {
-	xdpGUID := windows.GUID{
-		Data1: 0xf1832a85, Data2: 0x85d5, Data3: 0x45b0,
-		Data4: [...]byte{0x98, 0xa0, 0x70, 0x69, 0xd6, 0x30, 0x13, 0xb0},
-	}
-
-	_, err := WindowsProgramTypeForGUID("{00000000-0000-0000-0000-000000000001}")
-	qt.Assert(t, qt.ErrorIs(err, os.ErrNotExist))
-
-	programType, err := WindowsProgramTypeForGUID(xdpGUID.String())
-	qt.Assert(t, qt.IsNil(err))
-	qt.Assert(t, qt.Equals(WindowsXDP, programType))
-}
-
 func TestWindowsAttachTypeForGUID(t *testing.T) {
 	xdpGUID := windows.GUID{
 		Data1: 0x85e0d8ef, Data2: 0x579e, Data3: 0x4931,
